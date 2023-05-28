03d6874f7749ea443768b42455a63cac834355ed
Algoberry <72118211+algoberry@users.noreply.github.com>
2023-05-28 22:35:14 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once("config.php");
 include_once("WebCrawler.php");
 
 $obj = new WebCrawler();
-$data = $obj->parser("https://www.forbes.com/advisor/business/best-designed-websites/");
+$data = $obj->parser("https://www.algoberry.com");
 echo "<pre>";
 print_r($data);
 echo "</pre>";
